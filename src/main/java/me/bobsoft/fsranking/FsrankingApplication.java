910e055da52f46cd269421fcb1817495a648284f
Tay0108 <kjkosecki@gmail.com>
2018-08-17 10:29:22 +0200

    Spring Initializr

---
+++ src/main/java/me/bobsoft/fsranking/FsrankingApplication.java
@@ -0,0 +1,12 @@
+package me.bobsoft.fsranking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsrankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FsrankingApplication.class, args);
+	}
+}
---
+++ src/test/java/me/bobsoft/fsranking/FsrankingApplicationTests.java
@@ -0,0 +1,16 @@
+package me.bobsoft.fsranking;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FsrankingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
