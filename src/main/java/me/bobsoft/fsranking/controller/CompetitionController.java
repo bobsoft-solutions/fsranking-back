6a635edfbe36156d0771e098cf4f0b0341bb1de2
Monika Baran <monikabaranns@gmail.com>
2018-09-06 12:53:20 +0200

    GET request for /players, /players/{id}, /competitions/{id} done

--- src/main/java/me/bobsoft/fsranking/controller/CompetitionController.java
+++ src/main/java/me/bobsoft/fsranking/controller/CompetitionController.java
@@ -4,9 +4,11 @@ import me.bobsoft.fsranking.model.Competition;
 import me.bobsoft.fsranking.service.CompetitionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class CompetitionController {
@@ -17,5 +19,10 @@ public class CompetitionController {
     @GetMapping("/competitions")
     public List<Competition> findAllCompetitions() {
         return competitionService.findAll();
+    }
+
+    @GetMapping("/competitions/{id}")
+    public Optional<Competition> findPlayerById(@PathVariable Integer id) {
+        return competitionService.findById(id);
     }
 }
--- src/main/java/me/bobsoft/fsranking/service/CompetitionService.java
+++ src/main/java/me/bobsoft/fsranking/service/CompetitionService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CompetitionService {
@@ -15,5 +16,9 @@ public class CompetitionService {
 
     public List<Competition> findAll() {
         return competitionRepository.findAll();
+    }
+
+    public Optional<Competition> findById(Integer id) {
+        return competitionRepository.findById(id);
     }
 }
