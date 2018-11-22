a3eccebe62406a0ac805a73190dd73d1d56cc5c5
monikanana1 <monikabaranns@gmail.com>
2018-11-22 17:49:09 +0100

    getting all players ids that took part in any category

--- src/main/java/me/bobsoft/fsranking/controller/RankingCategory.java
+++ src/main/java/me/bobsoft/fsranking/controller/RankingCategory.java
@@ -3,6 +3,7 @@ package me.bobsoft.fsranking.controller;
 import me.bobsoft.fsranking.service.RankingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,8 +12,8 @@ public class RankingCategory {
     @Autowired
     private RankingService rankingService;
 
-    @GetMapping("/ranking/battle")
-    public Iterable<Integer> getPlayersOfBattleCategory() {
-        return rankingService.findPlayerAndSummaryScore("battle");
+    @GetMapping("/ranking/{category}")
+    public Iterable<Integer> getPlayersOfCategory(@PathVariable String category) {
+        return rankingService.findPlayerAndSummaryScore(category);
     }
 }
