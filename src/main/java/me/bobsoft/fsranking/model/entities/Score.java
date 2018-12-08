09e8f62460bbec1814e393edf3351110e5678dc0
monikanana1 <monikabaranns@gmail.com>
2018-12-08 16:41:08 +0100

    added attribute "id" in score and made it only one PK

--- src/main/java/me/bobsoft/fsranking/model/entities/Score.java
+++ src/main/java/me/bobsoft/fsranking/model/entities/Score.java
@@ -1,24 +1,24 @@
 package me.bobsoft.fsranking.model.entities;
 
 import lombok.Data;
-import me.bobsoft.fsranking.model.utils.ScoreIdClass;
 
 import javax.persistence.*;
 
 @Data
 @Entity
 @Table(name = "score")
-@IdClass(ScoreIdClass.class)
 public class Score {
+
+    @Id
+    @Column(name = "id")
+    private Integer id;
 
     @ManyToOne
     @JoinColumn(name = "id_player")
-    @Id
     private Player player;
 
     @ManyToOne
     @JoinColumn(name = "id_competition")
-    @Id
     private Competition competition;
 
     @ManyToOne
@@ -27,9 +27,8 @@ public class Score {
 
     @Column(name = "score")
     private Integer score;
-    
+
     @ManyToOne
     @JoinColumn(name = "id_category")
-    @Id
     private Category category;
 }
