ee20bc0d68fe3e666e448612807bfd818c3a982c
monikanana1 <monikabaranns@gmail.com>
2018-12-12 23:52:18 +0100

    @CrossOrigin on new controllers

--- src/main/java/me/bobsoft/fsranking/controller/CategoryController.java
+++ src/main/java/me/bobsoft/fsranking/controller/CategoryController.java
@@ -3,11 +3,13 @@ package me.bobsoft.fsranking.controller;
 import me.bobsoft.fsranking.model.entities.Category;
 import me.bobsoft.fsranking.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 public class CategoryController {
 
