7f2b5e9c60c61ba6d3c803294ccfc63c9df8c69a
Albert Duz <dddd.albert@gmail.com>
2018-12-29 00:33:25 +0100

    spanish ver

---
+++ src/main/java/me/bobsoft/fsranking/config/WebSecurityConfiguration.java
@@ -0,0 +1,56 @@
+package me.bobsoft.fsranking.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@EnableWebSecurity
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+
+        UserDetails user=User.builder().username("user").password(passwordEncoder().encode("secret")).
+                roles("USER").build();
+        UserDetails userAdmin=User.builder().username("admin").password(passwordEncoder().encode("secret")).
+                roles("ADMIN").build();
+        return new InMemoryUserDetailsManager(user,userAdmin);
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new  BCryptPasswordEncoder();
+    }
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/","/index","/webpublico").permitAll()
+                .antMatchers("/webprivado").authenticated()
+                .antMatchers("/webadmin").hasRole("ADMIN").and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout() // Metodo get pues he desabilitado CSRF
+                .permitAll();
+    }
+
+}
